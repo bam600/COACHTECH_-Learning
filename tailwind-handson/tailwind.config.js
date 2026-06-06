9945dc3a151aa68dd1a8958f2ae3268a21c4ef81
hitomi sasanuma <chitomi4866@icloud.com>
2026-06-07 08:19:59 +0900

    学習用フォルダ初回登録

---
+++ tailwind-handson/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // 変更箇所
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
