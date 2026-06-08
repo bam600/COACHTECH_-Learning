1401de3ff76ecd6773899a238bad4a527b12a740
hitomi sasanuma <chitomi4866@icloud.com>
2026-06-09 06:30:13 +0900

    if文

--- .vscode/4_javascript/training.js
+++ .vscode/4_javascript/training.js
@@ -1,8 +1,19 @@
-// 論理演算子
+// if文
 
-const user = null;
+// 基本構文
+// if(条件式){
+// 条件式がtrueの場合に実行される処理
+// }
 
-// userがnullなので、user.isAdminの評価は実行されず、エラーにならない
-if (user && user.isAdmin) {
-  console.log("管理者です");
+const score = 80;
+
+if (score >= 60) {
+  // 合格ですと表される
+  console.log("合格です！");
+}
+
+const age = 21;
+if (age < 20) {
+  // 表示されない
+  console.log("未成年です");
 }
