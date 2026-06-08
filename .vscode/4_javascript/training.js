8f539a17e535597268a818597e5d0b00ea50cfba
hitomi sasanuma <chitomi4866@icloud.com>
2026-06-09 06:49:07 +0900

    else文

--- .vscode/4_javascript/training.js
+++ .vscode/4_javascript/training.js
@@ -1,19 +1,16 @@
-// if文
+// else文
 
 // 基本構文
 // if(条件式){
 // 条件式がtrueの場合に実行される処理
-// }
+// }else{
+// 条件式がfalseの場合に実行される処理
+//}
 
-const score = 80;
+const age = 22;
 
-if (score >= 60) {
-  // 合格ですと表される
-  console.log("合格です！");
-}
-
-const age = 21;
 if (age < 20) {
-  // 表示されない
-  console.log("未成年です");
+  console.log("未成年です入場できません");
+} else {
+  console.log("成人です。どうぞおはいりください。");
 }
