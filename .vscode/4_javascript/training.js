4ed80e6d362eb4833b8c8e26db2bd89102e9ee33
hitomi sasanuma <chitomi4866@icloud.com>
2026-06-09 06:09:10 +0900

    スコープの基本2

--- .vscode/4_javascript/training.js
+++ .vscode/4_javascript/training.js
@@ -1,15 +1,5 @@
 // ６．スコープの基本
 
-function example() {
-  const message = "関数内の変数";
-  // OK
-  console.log(message);
-}
-
-example();
-// エラー！関数外からはアクセスできない
-console.log(message);
-
 // グローバル変数は関数内からアクセスできる
 const globalMessage = "グローバル変数";
 
@@ -19,4 +9,4 @@ function showMessage() {
 }
 
 // グローバル変数が表示
-showMessage();
+// showMessage();
