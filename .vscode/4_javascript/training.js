c619106113e845c2f83ce05e690a7d1c009d5ec3
hitomi sasanuma <chitomi4866@icloud.com>
2026-06-09 06:06:50 +0900

    スコープの基本

--- .vscode/4_javascript/training.js
+++ .vscode/4_javascript/training.js
@@ -1,16 +1,22 @@
-function greet(name = "ゲスト") {
-  console.log(`こんにちは、${name}さん`);
+// ６．スコープの基本
+
+function example() {
+  const message = "関数内の変数";
+  // OK
+  console.log(message);
 }
-// nameの値がないと判断したらゲストさんが表示される
-greet();
-greet("太郎");
 
-// アロー関数でも同様　mutiply ＝掛け算の意味
-const mutiply = (a, b = 1) => a * b;
+example();
+// エラー！関数外からはアクセスできない
+console.log(message);
 
-// bのみ初期値を設定しているためnanが表示される
-console.log(mutiply());
-// 15
-console.log(mutiply(5, 3));
-// 5
-console.log(mutiply(5));
+// グローバル変数は関数内からアクセスできる
+const globalMessage = "グローバル変数";
+
+function showMessage() {
+  // OK
+  console.log(globalMessage);
+}
+
+// グローバル変数が表示
+showMessage();
