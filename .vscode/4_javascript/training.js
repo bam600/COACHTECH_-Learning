11b4543fe1b2484f6e6bc97cd50065ea4f79a7aa
hitomi sasanuma <chitomi4866@icloud.com>
2026-06-09 16:54:57 +0900

    三項演算子

--- .vscode/4_javascript/training.js
+++ .vscode/4_javascript/training.js
@@ -1,23 +1,15 @@
-// else if文
+// 三項演算子
 
-// 基本構文
-// if(条件式){
-// 条件式がtrueの場合に実行される処理]
-// }else if(条件式2){
-// 条件式1がfalseで条件式2がtrueの場合の処理
+// //if-else文の場合
+const age = 20;
+
+// let message;
+// if (age >= 20) {
+//   message = "成人";
+// } else {
+//   message = "未成年";
 // }
-// }else{
-// 条件式がfalseの場合に実行される処理
-//}
 
-const score = 40;
-
-if (score >= 90) {
-  console.log("優");
-} else if (score >= 70) {
-  console.log("良");
-} else if (score >= 50) {
-  console.log("可");
-} else {
-  console.log("不可");
-}
+// 三項演算子の場合
+const message = age >= 20 ? "成人" : "未成年";
+console.log(message);
