e447f659e141e61e92838915f9d324a6fa8d299b
hitomi sasanuma <chitomi4866@icloud.com>
2026-06-11 06:10:45 +0900

    // 4-2-1: 配列の基本操作（push, pop, map, filter）

--- .vscode/4_javascript/training.js
+++ .vscode/4_javascript/training.js
@@ -1,10 +1,22 @@
 // 4-2-1: 配列の基本操作（push, pop, map, filter）
 
-// 配列の作成
-const fruits = ["apple", "Banana", "Cherry"];
+const numbers = [1, 2, 3];
+numbers.push(4);
 
-// インデックスを使ったアクセス
-// 配列0番目を表示
-console.log(fruits[0]);
-// 配列数の表示
-console.log(fruits.length);
+// 4が追加される
+console.log(numbers);
+
+// 末尾の４を返して削除
+const lastNumber = numbers.pop();
+// 4が削除される
+console.log(lastNumber);
+
+const numbers1 = [1, 2, 3, 4, 5];
+
+// 各要素を2倍にした新しい配列を作成
+const doubled = numbers1.map((num) => num * 2);
+console.log(doubled);
+
+// 偶数だけを集めた新しい配列を作成
+const evens = numbers1.filter(num => num % 2 === 0);
+console.log(evens);
