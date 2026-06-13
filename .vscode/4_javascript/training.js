fd1405e0f95dabb112a609a8976bdf2d0d9dbf0e
Hitomi <chitomi4866@icloud.com>
2026-06-13 14:03:49 +0900

    要素の取得と変更

--- .vscode/4_javascript/training.js
+++ .vscode/4_javascript/training.js
@@ -1,29 +1,15 @@
-// 4-2-4: 実践チャプターハンズオン: ToDoリストのデータ構造を作成する
-// 1. ToDoリストのデータ構造
-// 配列変数todosを作成　idプロパティ、textプロパティ、真偽値プロパティを作成
-let todos = [
-  { id: 1, text: "JavaScriptを学ぶ", completed: true },
-  { id: 2, text: "Reactを学ぶ", completed: false },
-  { id: 3, text: "Next.jsを学ぶ", completed: false },
-];
+// 4-3-2: 要素の取得と変更（querySelector, innerHTML）
 
-// 2. 未完了のToDoだけをフィルタリングする関数
-function getIncompleteTodos(todoList) {
-  return todoList.filter((todo) => !todo.completed);
-}
+// ID要素を取得
+const mainTitle = document.querySelector("#main-title");
 
-console.log("未完了のToDo:", getIncompleteTodos(todos));
+// クラスで最初の要素を取得
+const firstItem = document.querySelector(".list-item");
 
-// 3. 指定されたIDのToDoを完了済みにする関数
-//
-function completeTodo(todoList, todoId) {
-  return todoList.map((todo) => {
-    if (todo.id === todoId) {
-      return { ...todo, completed: true };
-    }
-    return todo;
-  });
-}
-// completeTodoメソッドにに配列todoのid2の情報を渡して処理を行う
-todos = completeTodo(todos, 2);
-console.log("更新後のToDoリスト:", todos);
+// クラスで全ての要素を取得
+const allItems = document.querySelectorAll(".list-item");
+
+// allItemsはNodeListなので、forEachでループ処理できる
+allItems.forEach((item) => {
+  console.log(item.textContent);
+});
