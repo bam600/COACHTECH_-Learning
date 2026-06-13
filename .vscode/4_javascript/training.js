1fae0ea9e3fbc3259bbd6daca559417a7403ba1d
Hitomi <chitomi4866@icloud.com>
2026-06-14 06:47:40 +0900

    4-3-4: 実践 � チャプターハンズオン: ToDoリストを作成する

--- .vscode/4_javascript/training.js
+++ .vscode/4_javascript/training.js
@@ -1,11 +1,36 @@
-// 4-3-3: イベントリスナーの追加（addEventListener）
+// 4-3-4: 実践 チャプターハンズオン: ToDoリストを作成する
 
-const myButton = document.querySelector("#my-button");
+// 1. DOM要素の取得
+// todo-inputの要素を探す
+const todoInput = document.querySelector("#todo-input");
+// add-buttonの要素を探す
+const addButton = document.querySelector("#add-button");
+// todo-listの要素を探す
+const todoList = document.querySelector("#todo-list");
 
-function handleClick() {
-  alert("ボタンがクリックされました！");
-}
+// 2.イベントリスナーの登録
+// 追加ボタンをクリックしたら処理を行う
+addButton.addEventListener("click", function () {
+  // a.テキストの取得
+  // idがtodoInputの値を取得して定数todoTextに格納
+  const todoText = todoInput.value;
 
-// ボタンがクリックされたらhandleClick関数を実行
-myButton.addEventListener("click", handleClick);
-console.log(myButton);
+  // 入力が空の場合は何もしない
+  // todoTextが空だったら処理終了
+  if (todoText === "") return;
+
+  // b.li要素の作成
+  // todoTextに値がはいっていたら(falseの場合)liタグを作成
+  const newTodoItem = document.createElement("li");
+
+  // c.テキストの設定
+  // liタグにテキストを設定
+  newTodoItem.textContent = todoText;
+
+  // d.li要素の追加
+  todoList.appendChild(newTodoItem);
+
+  // e.inputを空にする
+  // 初期状態にもどす
+  todoInput.value = "";
+});
