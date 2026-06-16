8ed6c372c53c272ca1fc2b7901bd3feba48924a7
Hitomi <chitomi4866@icloud.com>
2026-06-17 07:06:38 +0900

    // await演算子

--- .vscode/4_javascript/training.js
+++ .vscode/4_javascript/training.js
@@ -1,16 +1,25 @@
 // 5-2-1: async/awaitの使い方
 
-// // async関数は常にPromiseを返す
-// async function greet() {
-//   return "こんにちは";
-// }
+// await演算子
 
-// 以下と同じ意味
-function greet() {
-  return Promise.resolve("こんにちは");
+// Promiseを返す関数
+function fetchData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ name: "太郎", age: 25 });
+    }, 1000);
+  });
 }
 
-// 使い方
-greet().then((message) => {
-  console.log(message);
+// then を使った書き方
+fetchData().then((data) => {
+  console.log(data);
 });
+
+// async/await を使った書き方
+async function getData() {
+  const data = await fetchData();
+  // Promiseが解決されるまで待つ
+  console.log(data);
+}
+getData();
