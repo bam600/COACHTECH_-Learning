ecc6151b58467598881969d3eb682c694fc7aaee
Hitomi <chitomi4866@icloud.com>
2026-06-17 07:25:59 +0900

    3. Promiseチェーンとの比較

--- .vscode/4_javascript/training.js
+++ .vscode/4_javascript/training.js
@@ -1,25 +1,36 @@
 // 5-2-1: async/awaitの使い方
 
-// await演算子
-
-// Promiseを返す関数
-function fetchData() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ name: "太郎", age: 25 });
-    }, 1000);
+// Promiseチェーン版
+getUser(1)
+  .then((user) => {
+    console.log("ユーザー:", user.name);
+    return getPosts(user.id);
+  })
+  .then((posts) => {
+    console.log("投稿数:", posts.length);
+    return getComments(posts[0].id);
+  })
+  .then((comments) => {
+    console.log("コメント数:", comments.length);
+  })
+  .catch((error) => {
+    console.error("エラー:", error);
   });
+
+// async/await版（より直感的！）
+async function fetchAllData() {
+  try {
+    const user = await getUser(1);
+    console.log("ユーザー:", user.name);
+
+    const posts = await getPosts(user.id);
+    console.log("投稿数:", posts.length);
+
+    const comments = await getComments(posts[0].id);
+    console.log("コメント数:", comments.length);
+  } catch (error) {
+    console.error("エラー:", error);
+  }
 }
 
-// then を使った書き方
-fetchData().then((data) => {
-  console.log(data);
-});
-
-// async/await を使った書き方
-async function getData() {
-  const data = await fetchData();
-  // Promiseが解決されるまで待つ
-  console.log(data);
-}
-getData();
+fetchAllData();
