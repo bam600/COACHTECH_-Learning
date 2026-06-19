f53c1463f3e8409c478eee00fc0768ba776aaab5
Hitomi <chitomi4866@icloud.com>
2026-06-20 08:56:31 +0900

    //5-2-2: fetch APIの基本 // レスポンスボディの取得

--- .vscode/4_javascript/training.js
+++ .vscode/4_javascript/training.js
@@ -1,11 +1,18 @@
 //5-2-2: fetch APIの基本
-
+// レスポンスボディの取得
 async function fetchData() {
-  // JSONPlaceholderの投稿(posts)を1件取得する
   const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
 
-  console.log(response);
-  // Response { type: "cors", url: "...", status: 200, ok: true, ... }
+  // レスポンスが成功したか確認
+  if (!response.ok) {
+    console.error("データの取得に失敗しました");
+    return;
+  }
+
+  // レスポンスボディをJSONとして取得
+  const post = await response.json();
+  console.log(post);
+  // { userId: 1, id: 1, title: "...", body: "..." }
 }
 
 fetchData();
