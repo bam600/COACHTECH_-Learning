f0ef6aab5ba10e354694edacba3e213040204dbd
hitomi sasanuma <chitomi4866@icloud.com>
2026-06-19 17:56:33 +0900

    5-2-2: fetch APIの基本

--- .vscode/4_javascript/training.js
+++ .vscode/4_javascript/training.js
@@ -1,53 +1,11 @@
-// 5-2-1: async/awaitの使い方
+//5-2-2: fetch APIの基本
 
-// // Promiseチェーン版
-// getUser(1)
-//   .then((user) => {
-//     console.log("ユーザー:", user.name);
-//     return getPosts(user.id);
-//   })
-//   .then((posts) => {
-//     console.log("投稿数:", posts.length);
-//     return getComments(posts[0].id);
-//   })
-//   .then((comments) => {
-//     console.log("コメント数:", comments.length);
-//   })
-//   .catch((error) => {
-//     console.error("エラー:", error);
-//   });
+async function fetchData() {
+  // JSONPlaceholderの投稿(posts)を1件取得する
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
 
-// // async/await版（より直感的！）
-// async function fetchAllData() {
-//   try {
-//     const user = await getUser(1);
-//     console.log("ユーザー:", user.name);
-
-//     const posts = await getPosts(user.id);
-//     console.log("投稿数:", posts.length);
-
-//     const comments = await getComments(posts[0].id);
-//     console.log("コメント数:", comments.length);
-//   } catch (error) {
-//     console.error("エラー:", error);
-//   }
-// }
-
-// fetchAllData();
-
-// 4.エラーハンドリング
-async function fetchUser(id) {
-  try {
-    const response = await fetch(`http://api.example.com/users/${id}`);
-
-    if (!response.ok) {
-      throw new Error("ユーザーが見つかりせん");
-    }
-
-    const user = await response.json();
-    return user;
-  } catch (error) {
-    console.error("エラー:", error.message);
-    return null;
-  }
+  console.log(response);
+  // Response { type: "cors", url: "...", status: 200, ok: true, ... }
 }
+
+fetchData();
