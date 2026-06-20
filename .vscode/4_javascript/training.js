bcf4b1deb39519657eeedd8b2cc5850dd5452124
Hitomi <chitomi4866@icloud.com>
2026-06-20 09:23:11 +0900

    5-2-3: JSONデータの取得と表示

--- .vscode/4_javascript/training.js
+++ .vscode/4_javascript/training.js
@@ -1,18 +1,27 @@
-//5-2-2: fetch APIの基本
-// レスポンスボディの取得
-async function fetchData() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+//5-2-3: JSONデータの取得と表示
 
-  // レスポンスが成功したか確認
-  if (!response.ok) {
-    console.error("データの取得に失敗しました");
-    return;
-  }
+async function displayUsers() {
+  // 定数 userListにidがuser-listを探して取得
+  const userList = document.querySelector("#user-list");
 
-  // レスポンスボディをJSONとして取得
-  const post = await response.json();
-  console.log(post);
-  // { userId: 1, id: 1, title: "...", body: "..." }
+  // 1. データを取得する
+  // 定数responseにfetch("https://jsonplaceholder.typicode.com/users")でデータをくださいと依頼して
+  // awaitで取得が完了するまでまつ
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  // 定数 usersに取得したデータを格納するまでまち、完了したら格納する
+  const users = await response.json();
+
+  // 2. 取得したユーザーデータの配列をループ処理
+  // 格納したデータ文ループ処理を行う
+  users.forEach((user) => {
+    // 3. 各ユーザーごとにli要素を生成
+    const listItem = document.createElement("li");
+    // li要素にuser.nameを追加していく
+    listItem.textContent = user.name;
+
+    // 4. ul要素にli要素を追加
+    userList.appendChild(listItem);
+  });
 }
 
-fetchData();
+displayUsers();
